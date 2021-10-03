27a4f2b5b4dbb8a64cf36ca2c5c6b0c806557e13
Camila Cocuzza <cami_cocu@hotmail.com>
2021-10-03 04:11:53 -0300

    negocios factura y detalles

---
+++ CLASE05/Negocios/NE_DetalleFactura_Articulos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CLASE05.Clases;
+
+namespace CLASE05.Negocios
+{
+    class NE_DetalleFactura_Articulos
+    {
+        BE_Acceso_Datos _BD = new BE_Acceso_Datos();
+
+        public void CargarCombo(ref ComboBox01 combo)
+        {
+            combo.DisplayMember = "nombre";
+            combo.ValueMember = "cod_articulo";
+            combo.DataSource = _BD.EjecutarSelect("SELECT nombre, cod_articulo FROM articulo");
+        }
+        public string RecuperarPrecioArticulo(string cod_articulo)
+        {
+            string sql = "SELECT precio FROM articulo WHERE cod_articulo = " + cod_articulo;
+            return _BD.EjecutarSelect(sql).Rows[0][0].ToString();
+        }
+    }
+
+}
---
+++ CLASE05/Negocios/NE_DetalleFactura_EquiposEnsamblados.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CLASE05.Clases;
+
+namespace CLASE05.Negocios
+{
+    class NE_DetalleFactura_EquiposEnsamblados
+    {
+        BE_Acceso_Datos _BD = new BE_Acceso_Datos();
+
+        public void CargarCombo(ref ComboBox01 combo)
+        {
+            combo.DisplayMember = "cod_prod_ensamblado";
+            combo.ValueMember = "cod_prod_ensamblado";
+            combo.DataSource = _BD.EjecutarSelect("SELECT cod_prod_ensamblado FROM producto_ensamblado");
+        }
+        public string RecuperarPrecioProdEnsamblado(string cod_prod_ensamblado)
+        {
+            string sql = "SELECT precio FROM producto_ensamblado WHERE cod_prod_ensamblado = " + cod_prod_ensamblado;
+            return _BD.EjecutarSelect(sql).Rows[0][0].ToString();
+        }
+    }
+}
