ffbf407a482dece1f1cbd78168bd5946a47926a0
Camila Cocuzza <cami_cocu@hotmail.com>
2021-10-06 20:14:41 -0300

    .

---
+++ SQLQuery4.sql
@@ -0,0 +1,5 @@
+﻿SELECT s.cantidad 
+                          FROM articulo a JOIN stock s ON a.cod_articulo = s.cod_articulo
+                          WHERE a.cod_articulo = '1' 
+                          AND s.fecha = (SELECT max(s1.fecha) FROM stock s1
+                                         WHERE s1.cod_articulo = s.cod_articulo);
